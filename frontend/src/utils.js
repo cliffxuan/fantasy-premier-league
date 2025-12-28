6ef33580e6057ceb260690957bb3d0cc192648d4
Cliff Xuan <cliff.xuan@gmail.com>
2025-12-28 14:38:01 +0000

    fix: update player image URLs for consistency
    - Changed player image URL format in getPlayerImage function.
    - Adjusted error handling in handlePlayerImageError to reflect new URL structure.

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,11 +1,11 @@
 export const getPlayerImage = (code, size = "110x140") =>
-  `https://resources.premierleague.com/premierleague/photos/players/${size}/p${code}.png`;
+  `https://resources.premierleague.com/premierleague25/photos/players/${size}/${code}.png`;
 
 export const handlePlayerImageError = (e, player, size = "110x140") => {
   const currentSrc = e.target.src;
   if (currentSrc.includes("premierleague/photos")) {
-    // Try premierleague25 without 'p'
-    e.target.src = `https://resources.premierleague.com/premierleague25/photos/players/${size}/${player.code}.png`;
+    // Try premierleague with 'p'
+    e.target.src = `https://resources.premierleague.com/premierleague/photos/players/${size}/p${player.code}.png`;
   } else {
     // Fallback to shirt (if it was some other url)
     e.target.src = `https://fantasy.premierleague.com/dist/img/shirts/standard/shirt_${player.team_code}-66.png`;
